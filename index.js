3b81407ace9fe9dcb3ba79ed131318b9653d6152
Guilherme Braga <bragaguilherme152@yahoo.com.br>
2022-05-18 17:40:43 -0300

    metodo push

--- index.js
+++ index.js
@@ -28,15 +28,41 @@ arrFill.map((item, index) => item * 2)
 
 const map3 = arrFill.map((item, index) => 
 {
+  if (index == 0) {
   return (
   "O novo item é " + item * 2 + " e o índice é " + index
-  //  item * 2
    )
-})
+  }
+  else {
+    return console.log("Não encontrei")
+  }
+}
+)
 
 console.log(map3)
 
 //Push
+
+const arrA = ["gui", "dev"]
+
+const arrB = ["youtuber", "programador"]
+
+console.log(arrA, arrB)
+
+// const testePush = arrB.map((item) => arrA.push(item))
+const testePush = arrA.push(1, 2, 3, true)
+
+console.log(testePush) //Retorno do método modificado
+console.log(arrA) //Array modificado
+
+//Problema Comum
+let arrayVazio = []
+if (arrA.length > 0) {
+  const pessoa = {nome: "Guilherme", idade: 21}
+  arrayVazio.push(pessoa)
+}
+
+console.log(arrayVazio)
 
 //ForEach
 
